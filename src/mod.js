b3278a6b6c93fdc1e64fb86e9c295f88aafed3df
harmony <noemail@gmail.com>
2024-12-08 12:04:57 +0500

    first commit

---
+++ src/mod.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const namesConfig = require("../config/config.jsonc");
+
+class BotNames {
+    bearCFG = require("../names/bear.json");
+    usecCFG = require("../names/usec.json");
+    postDBLoad(container) {
+        // Get the logger from the server container.
+        const logger = container.resolve("WinstonLogger");
+        // Get database from server.
+        const db = container.resolve("DatabaseServer");
+        const bot = db.getTables().bots.types;
+        const bearNames = this.bearCFG['Names'];
+        const usecNames = this.usecCFG['Names'];
+        bot["bear"].firstName = bot["bear"].firstName;
+        bot["usec"].firstName = bot["bear"].firstName;
+        logger.info("Bot names loaded!");
+    }
+}
+module.exports = { mod: new BotNames() };
+//# sourceMappingURL=mod.js.map
