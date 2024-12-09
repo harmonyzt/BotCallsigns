693c0d5c60d086ee1bc658dc3a8f67e25f22e735
harmony <hattrix@yandex.ru>
2024-12-09 07:51:08 +0500

    support scav names

--- src/mod.js
+++ src/mod.js
@@ -2,19 +2,32 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 
 class BotNames {
-    bearCFG = require("../names/bear.json"); 
-    usecCFG = require("../names/usec.json");
+    CFG = require("../config/config.json");
+    bearNames = require("../names/bear.json"); 
+    usecNames = require("../names/usec.json");
+    scavNames = require("../names/scav.json");
+
     postDBLoad(container) {
         // Get the logger from the server container.
         const logger = container.resolve("WinstonLogger");
         // Get database from server.
         const db = container.resolve("DatabaseServer");
         const bot = db.getTables().bots.types;
-        const bearNames = this.bearCFG['Names'];
-        const usecNames = this.usecCFG['Names'];
+        const bearNames = this.bearNames['Names'];
+        const usecNames = this.usecNames['Names'];
         bot["bear"].firstName = bearNames;
         bot["usec"].firstName = usecNames;
-        logger.info("Bot names loaded!");
+
+        // If we should use custom SCAV names too.
+        if(this.CFG.useCustomScavNames){
+            const scavFirstNames = this.scavNames['firstNames'];
+            const scavLastNames = this.scavNames['lastNames'];
+            bot["assault"].firstName = scavFirstNames;
+            bot["assault"].lastName = scavLastNames;
+            logger.info("Custom SCAV names loaded!");
+        }
+
+        logger.info("Custom PMC names loaded!");
     }
 }
 module.exports = { mod: new BotNames() };
