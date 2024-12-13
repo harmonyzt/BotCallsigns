cec2c478c36919e521b0a8ae2a39f7c6ea270865
harmony <hattrix@yandex.ru>
2024-12-13 11:25:43 +0500

    add live mode for TTV-Players

--- src/mod.js
+++ src/mod.js
@@ -1,9 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require('fs');
 
 class BotNames {
     CFG = require("../config/config.json");
-    bearNames = require("../names/bear.json"); 
+    bearNames = require("../names/bear.json");
     usecNames = require("../names/usec.json");
     scavNames = require("../names/scav.json");
 
@@ -18,16 +19,35 @@ class BotNames {
         bot["bear"].firstName = bearNames;
         bot["usec"].firstName = usecNames;
 
+        if (this.CFG.liveMode) {
+            logger.log("[BotCallsigns | LIVE MODE] Live mode is ENABLED! This will generate a new file with all names for TTV Players every server start up. Be careful!", "yellow");
+            // Generating a file with ALL the BEAR and USEC names if the mod we support exists.
+            const pathToTTVPlayers = "./user/mods/TTV-Players";
+            if (fs.existsSync(pathToTTVPlayers)) {
+                var BEARStr = new Array(this.bearNames['Names']);
+                var USECStr = new Array(this.usecNames['Names']);
+                var allthenames = BEARStr.concat(USECStr);
+
+                const pathToAllNames = "./user/mods/TTV-Players/names/names.json";
+                fs.writeFile(pathToAllNames, JSON.stringify({ names: allthenames.flat() }, null, 2), (err) => {
+                    if (err) throw err;
+                    logger.log("[BotCallsigns | LIVE MODE] names.json for TTV Players mod was updated successfully!", "yellow");
+                });
+            } else {
+                logger.log("[BotCallsigns | LIVE MODE] Could not find TTV Players mod while live mode is enabled! Stopping the mod...", "red");
+                return;
+            }
+        }
         // If we should use custom SCAV names too.
-        if(this.CFG.useCustomScavNames){
+        if (this.CFG.useCustomScavNames) {
             const scavFirstNames = this.scavNames['firstNames'];
             const scavLastNames = this.scavNames['lastNames'];
             bot["assault"].firstName = scavFirstNames;
             bot["assault"].lastName = scavLastNames;
-            logger.info("Custom SCAV names loaded!");
+            logger.info("[BotCallsigns] Custom SCAV names loaded!");
         }
 
-        logger.info("Custom PMC names loaded!");
+        logger.info("[BotCallsigns] Custom PMC names loaded!");
     }
 }
 module.exports = { mod: new BotNames() };
