aaec788540b47637944ff92e265c247a70544f8e
bushtail <evannosich@icloud.com>
2025-12-17 18:10:54 -0500

    Add user defined bot config

---
+++ Models/UserDefinedBotCallsignNames.cs
@@ -0,0 +1,7 @@
+﻿namespace BotCallsigns.Models;
+
+public class UserDefinedBotCallsignNames
+{
+    public List<string> USECNames { get; set; } = [];
+    public List<string> BEARNames { get; set; } = [];
+}
