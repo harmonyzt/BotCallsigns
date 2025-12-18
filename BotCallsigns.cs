bd0122252ba8ead32c8fc7243945a9b981aa914f
John Doe <hattrix@yandex.ru>
2025-12-19 00:17:02 +0500

    Bump version

--- BotCallsigns.cs
+++ BotCallsigns.cs
@@ -18,7 +18,7 @@ public record ModMetadata : AbstractModMetadata
     public override string Name { get; init; } = "Bot Callsigns";
     public override string Author { get; init; } = "harmony";
     public override List<string>? Contributors { get; init; } = ["yuyui.moe", "Helldiver"];
-    public override SemanticVersioning.Version Version { get; init; } = new("2.0.0");
+    public override SemanticVersioning.Version Version { get; init; } = new("2.0.2");
     public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
     public override List<string>? Incompatibilities { get; init; }
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
